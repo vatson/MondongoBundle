7d9fa69d61b6a4422b9aad2a0710cf4c06913f89
pablodip <pablodip@gmail.com>
2010-12-02 13:44:10 +0100

    changed to use gen class in schema

--- Command/GenerateCommand.php
+++ Command/GenerateCommand.php
@@ -54,33 +54,35 @@ class GenerateCommand extends Command
     {
         $output->writeln('processing config classes');
 
+        $genDir = $this->container->getParameter('kernel.root_dir').'/../src/Gen';
+
         $configClasses = array();
         foreach ($this->container->get('kernel')->getBundles() as $bundle) {
-            $bundleClass     = get_class($bundle);
-            $bundleNamespace = substr($bundleClass, 0, strrpos($bundleClass, '\\'));
-            $bundleName      = substr($bundleClass, strrpos($bundleClass, '\\') + 1);
+            $bundleClass        = get_class($bundle);
+            $bundleName         = substr($bundleClass, strrpos($bundleClass, '\\') + 1);
+            $bundleGenNamespace = 'Gen\\'.$bundleName;
 
             if (is_dir($dir = $bundle->getPath().'/Resources/config/mondongo')) {
                 $finder = new Finder();
                 foreach ($finder->files()->name('*.yml')->followLinks()->in($dir) as $file) {
                     foreach ((array) Yaml::load($file) as $class => $configClass) {
                         // class
-                        if (0 === strpos($class, $bundleNamespace)) {
+                        if (0 === strpos($class, $bundleGenNamespace)) {
                             if (
-                                0 !== strpos($class, $bundleNamespace.'\\Document')
+                                0 !== strpos($class, $bundleGenNamespace.'\\Document')
                                 ||
-                                strlen($bundleNamespace.'\\Document') !== strrpos($class, '\\')
+                                strlen($bundleGenNamespace.'\\Document') !== strrpos($class, '\\')
                             ) {
                                 throw new \RuntimeException(sprintf('The class "%s" is not in the Document namespace of the bundle.', $class));
                             }
                         }
 
                         // outputs && bundle
-                        if (0 === strpos($class, $bundleNamespace)) {
-                            $configClass['output'] = $bundle->getPath().'/Document';
+                        if (0 === strpos($class, $bundleGenNamespace)) {
+                            $configClass['output'] = $genDir.'/'.$bundleName.'/Document';
 
-                            $configClass['bundle_name'] = $bundleName;
-                            $configClass['bundle_dir']  = $bundle->getPath();
+                            $configClass['bundle_class'] = $bundleClass;
+                            $configClass['bundle_dir']   = $bundle->getPath();
                         } else {
                             unset($configClass['output'], $configClass['bundle_name'], $configClass['bundle_dir']);
                         }
@@ -101,9 +103,7 @@ class GenerateCommand extends Command
         $mondator->setConfigClasses($configClasses);
         $mondator->setExtensions(array(
             new \Mondongo\Extension\Core(),
-            new \Bundle\MondongoBundle\Extension\GenBundleDocument(array(
-                'gen_dir' => $this->container->getParameter('kernel.root_dir').'/../src/Gen',
-            )),
+            new \Bundle\MondongoBundle\Extension\GenBundleDocument(),
         ));
         $mondator->process();
     }
--- Extension/GenBundleDocument.php
+++ Extension/GenBundleDocument.php
@@ -36,19 +36,11 @@ class GenBundleDocument extends Extension
     /**
      * @inheritdoc
      */
-    protected function setup()
-    {
-        $this->addRequiredOption('gen_dir');
-    }
-
-    /**
-     * @inheritdoc
-     */
     protected function doProcess()
     {
-        foreach (array('bundle_name', 'bundle_dir') as $parameter) {
+        foreach (array('bundle_class', 'bundle_dir') as $parameter) {
             if (!isset($this->configClass[$parameter])) {
-                throw new \RuntimeException(sprintf('The class "%s" does not have the "%s" parameter.', $this->class, $parameter));
+                throw new \RuntimeException(sprintf('The class "%s" does not have the "%s" config class parameter.', $this->class, $parameter));
             }
         }
 
@@ -56,71 +48,37 @@ class GenBundleDocument extends Extension
          * Definitions.
          */
         $classes = array(
-            'document'          => 'Gen\%bundle_name%\Document\%class%',
-            'document_bundle'   => $this->definitions['document']->getClass(),
-            'document_base'     => 'Gen\%bundle_name%\Document\Base\%class%',
-            'repository'        => 'Gen\%bundle_name%\Document\%class%Repository',
-            'repository_bundle' => $this->definitions['repository']->getClass(),
-            'repository_base'   => 'Gen\%bundle_name%\Document\Base\%class%Repository',
+            'document_bundle'   => '%bundle_namespace%\Document\%class_name%',
+            'repository_bundle' => '%bundle_namespace%\Document\%class_name%Repository',
         );
         foreach ($classes as &$class) {
             $class = strtr($class, array(
-                '%bundle_name%' => $this->configClass['bundle_name'],
-                '%class%'       => substr($this->class, strrpos($this->class, '\\') + 1),
+                '%bundle_namespace%' => substr($this->configClass['bundle_class'], 0, strrpos($this->configClass['bundle_class'], '\\')),
+                '%class_name%'       => substr($this->class, strrpos($this->class, '\\') + 1),
             ));
         }
 
         // document
-        $this->definitions['document']->setClass($classes['document']);
         $this->definitions['document']->setParentClass('\\'.$classes['document_bundle']);
-        $this->definitions['document']->setDocComment(<<<EOF
-/**
- * {$classes['document']} document.
- */
-EOF
-        );
 
         $this->definitions['document_bundle'] = new Definition($classes['document_bundle']);
-        $this->definitions['document_bundle']->setParentClass('\\'.$classes['document_base']);
+        $this->definitions['document_bundle']->setParentClass('\\'.$this->definitions['document_base']->getClass());
         $this->definitions['document_bundle']->setIsAbstract(true);
         $this->definitions['document_bundle']->setDocComment(<<<EOF
 /**
- * {$classes['document']} document bundle.
- */
-EOF
-        );
-
-        $this->definitions['document_base']->setClass($classes['document_base']);
-        $this->definitions['document_base']->setDocComment(<<<EOF
-/**
- * {$classes['document']} document base.
+ * {$this->class} document bundle.
  */
 EOF
         );
 
         // repository
-        $this->definitions['repository']->setClass($classes['repository']);
         $this->definitions['repository']->setParentClass('\\'.$classes['repository_bundle']);
-        $this->definitions['repository']->setDocComment(<<<EOF
-/**
- * {$classes['document']} document repository
- */
-EOF
-        );
 
         $this->definitions['repository_bundle'] = new Definition($classes['repository_bundle']);
-        $this->definitions['repository_bundle']->setParentClass('\\'.$classes['repository_base']);
+        $this->definitions['repository_bundle']->setParentClass('\\'.$this->definitions['repository_base']->getClass());
         $this->definitions['repository_bundle']->setDocComment(<<<EOF
 /**
- * {$classes['document']} document repository bundle
- */
-EOF
-        );
-
-        $this->definitions['repository_base']->setClass($classes['repository_base']);
-        $this->definitions['repository_base']->setDocComment(<<<EOF
-/**
- * {$classes['document']} document repository base
+ * {$this->class} document repository bundle
  */
 EOF
         );
@@ -128,27 +86,11 @@ EOF
         /*
          * Outputs.
          */
-        $dirs = array(
-            'document'        => '%gen_dir%/%bundle_name%/Document',
-            'document_bundle' => '%bundle_dir%/Document',
-            'document_base'   => '%gen_dir%/%bundle_name%/Document/Base',
-        );
-        foreach ($dirs as &$dir) {
-            $dir = strtr($dir, array(
-                '%gen_dir%'     => $this->getOption('gen_dir'),
-                '%bundle_dir%'  => $this->configClass['bundle_dir'],
-                '%bundle_name%' => $this->configClass['bundle_name'],
-            ));
-        }
 
         // document
-        $this->outputs['document']->setDir($dirs['document']);
-        $this->outputs['document_bundle'] = new Output($dirs['document_bundle']);
-        $this->outputs['document_base']->setDir($dirs['document_base']);
+        $this->outputs['document_bundle'] = new Output($this->configClass['bundle_dir'].'/Document');
 
         // repository
-        $this->outputs['repository']->setDir($dirs['document']);
-        $this->outputs['repository_bundle'] = new Output($dirs['document_bundle']);
-        $this->outputs['repository_base']->setDir($dirs['document_base']);
+        $this->outputs['repository_bundle'] = new Output($this->configClass['bundle_dir'].'/Document');
     }
 }
