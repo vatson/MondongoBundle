6d907f18020abcd1e095204bb20ed34fc57797f6
pablodip <pablodip@gmail.com>
2010-12-02 11:19:20 +0100

    updated collector to work with the new tag way

--- Resources/views/Profiler/mondongo_menu.php
+++
@@ -1,3 +0,0 @@
-<div class="count"><?php echo $data->getNbQueries() ?></div>
-<img style="margin: 0 5px 0 0; vertical-align: middle; width: 32px" width="32" height="32" alt="Mondongo" src="<?php echo $view->get('assets')->getUrl('bundles/webprofiler/images/db.png') ?>" />
-Mondongo
