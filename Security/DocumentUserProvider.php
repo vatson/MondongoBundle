eb2db674f3840401ad768bdfdfddb4fc8668fec3
pablodip <pablodip@gmail.com>
2010-12-02 10:24:32 +0100

    added user provider

---
+++ Security/DocumentUserProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * Copyright 2010 Pablo Díez Pascual <pablodip@gmail.com>
+ *
+ * This file is part of MondongoBundle.
+ *
+ * MondongoBundle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MondongoBundle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MondongoBundle. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Bundle\MondongoBundle\Logger;
+
+use Symfony\Component\Security\User\UserProviderInterface;
+use Symfony\Component\Security\Exception\UsernameNotFoundException;
+use Mondongo\Repository;
+
+/**
+ * DocumentUserProvider.
+ *
+ * @package MondongoBundle
+ * @author  Pablo Díez Pascual <pablodip@gmail.com>
+ */
+class DocumentUserProvider implements UserProviderInterface
+{
+    protected $repository;
+    protected $field;
+
+    /**
+     * Constructor.
+     *
+     * @param \Mondongo\Repository $repository A Mondongo repository.
+     * @param string               $field      The field.
+     */
+    public function __construct(Repository $repository, $class, $field = null)
+    {
+        $this->repository = $repository;
+        $this->field      = $field;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        if ($this->field) {
+            $user = $this->repository->findOne(array($this->field => $username));
+        } else {
+            if (!$this->repository instanceof UserProviderInterface) {
+                throw new \InvalidArgumentException(sprintf('The Moctrine repository "%s" must implement UserProviderInterface.', get_class($this->repository)));
+            }
+
+            $user = $this->repository->loadUserByUsername($username);
+        }
+
+        if (null === $user) {
+            throw new UsernameNotFoundException(sprintf('User "%s" not found.', $username));
+        }
+
+        return $user;
+    }
+}
